5252dd1f6a27963c53cf2c6e2bd5fa796c565043
Rakesh Reddy <lrr2567@outlook.com>
2023-11-17 13:51:20 -0700

    Start of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserRegistrationComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'SchoolApp';
---
+++ src/app/user-registration/user-registration.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-registration',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-registration.component.html',
+  styleUrl: './user-registration.component.css'
+})
+export class UserRegistrationComponent {
+
+}
